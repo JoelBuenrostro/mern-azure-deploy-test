11a7ef639274c544e945354ff816427111740db7
JoelBuenrostro <joel@deepia.mx>
2023-08-29 04:03:30 -0600

    DT-5 Creacion de usuario en base de datos
    Creacion de rutas login y register y envio a base de datos.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
 
+import authRoutes from './routes/auth.routes.js'
+
 const server = express()
 
 server.use(morgan('dev'))
+server.use(express.json())
+server.use("/api", authRoutes);
 
 export default server

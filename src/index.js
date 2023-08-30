bde900f278464e8d41bee0260593fad3d61aa0a8
JoelBuenrostro <joel@deepia.mx>
2023-08-30 06:37:38 -0600

    DT-8 Peticion de archivos estaticos por backend
    Se crea una peticion para mostrar los archivos estaticos desde el puerto backend

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import express from 'express'
 import server from './server.js'
+import path from 'path'
 import {connectDB} from './config/db.js'
 
 connectDB();
 server.listen(4000)
+
+
+//production script
+server.use(express.static("./client/dist"));
+server.get("*", (req, res) => {
+    res.sendFile(path.resolve("client", "dist", "index.html"))
+});
+
 console.log('servervidor en puerto', 4000)
